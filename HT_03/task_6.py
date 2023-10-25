2f45dc8b3dd00a297c3d71a13e0a7a7a62a76690
Maria <terentyevamarie@gmail.com>
2023-10-25 10:29:58 +0300

    modified task_06.py

--- HT_03/task_6.py
+++ HT_03/task_6.py
@@ -11,17 +11,38 @@ dict_1 = {
     'salary': 3750,
     'speed': 75.8,
     'file': 100644,
+    'e': [-12, -336],
+    'e1': 'Ra'
 }
 
-max_value = dict_1['id']
-min_value = dict_1['id']
+max_value = float('-inf')
+min_value = float('inf')
+max_value_str = None
+min_value_str = None
+max_value_list = float('-inf')
+min_value_list = float('inf')
 
 for key, value in dict_1.items():
     if isinstance(value, (int, float, bool)):
-        if max_value is None or value > max_value:
+        if value > max_value:
             max_value = value
-        if min_value is None or value < min_value:
+        if value < min_value:
             min_value = value
+    if isinstance(value, str):
+        if max_value_str is None or len(value) > len(max_value_str):
+            max_value_str = value
+        if min_value_str is None or len(value) < len(min_value_str):
+            min_value_str = value
+    if isinstance(value, list):
+        if max(value) > max_value_list:
+            max_value_list = max(value)
+        if min(value) < min_value_list:
+            min_value_list = min(value)
+
 
 print(f"Maximum value: {max_value}")
 print(f"Minimum value: {min_value}")
+print(f"Maximum str value: {max_value_str}")
+print(f"Minimum str value: {min_value_str}")
+print(f"Maximum value in list: {max_value_list}")
+print(f"Minimum value in list: {min_value_list}")
