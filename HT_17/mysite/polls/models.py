cab09d721f1a8cf98f7e2a3d31dbb56536566729
Maria <terentyevamarie@gmail.com>
2023-12-24 15:43:22 +0200

    changes

--- HT_17/mysite/polls/models.py
+++ HT_17/mysite/polls/models.py
@@ -17,8 +17,8 @@ class Question(models.Model):
     @admin.display(
         boolean=True,
         ordering="pub_date",
-        description="Published recently?",)
-
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
